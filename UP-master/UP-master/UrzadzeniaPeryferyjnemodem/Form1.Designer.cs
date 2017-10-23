afb23d1c8209d38573db4fadcc4eca331ce5db2f
magdalena-biernat <magdalena.biernat@poczta.fm>
2017-10-23 20:27:41 +0200

    Po zajęciach :)

---
+++ UP-master/UP-master/UrzadzeniaPeryferyjnemodem/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace UrzadzeniaPeryferyjnemodem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.PeripheralLabel = new System.Windows.Forms.Label();
+            this.MessageLabel = new System.Windows.Forms.Label();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxPorts = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 38);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(204, 323);
+            this.textBox1.TabIndex = 1;
+            // 
+            // PeripheralLabel
+            // 
+            this.PeripheralLabel.AutoSize = true;
+            this.PeripheralLabel.Location = new System.Drawing.Point(16, 22);
+            this.PeripheralLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.PeripheralLabel.Name = "PeripheralLabel";
+            this.PeripheralLabel.Size = new System.Drawing.Size(54, 13);
+            this.PeripheralLabel.TabIndex = 2;
+            this.PeripheralLabel.Text = "Peripheral";
+            // 
+            // MessageLabel
+            // 
+            this.MessageLabel.AutoSize = true;
+            this.MessageLabel.Location = new System.Drawing.Point(502, 41);
+            this.MessageLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.MessageLabel.Name = "MessageLabel";
+            this.MessageLabel.Size = new System.Drawing.Size(50, 13);
+            this.MessageLabel.TabIndex = 3;
+            this.MessageLabel.Text = "Message";
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(494, 105);
+            this.buttonSend.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(56, 19);
+            this.buttonSend.TabIndex = 4;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Location = new System.Drawing.Point(481, 301);
+            this.buttonDisconnect.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(70, 50);
+            this.buttonDisconnect.TabIndex = 5;
+            this.buttonDisconnect.Text = "Close";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(401, 301);
+            this.buttonConnect.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(68, 50);
+            this.buttonConnect.TabIndex = 6;
+            this.buttonConnect.Text = "Open";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "ATA ",
+            "ATB",
+            "ATB1",
+            "ATB2",
+            "ATD",
+            "ATE0",
+            "ATE1",
+            "ATH0",
+            "ATH1",
+            "ATI0",
+            "ATL0",
+            "ATL1",
+            "ATL2",
+            "ATL3",
+            "ATM0",
+            "ATM1",
+            "AT2",
+            "ATN0",
+            "ATN1",
+            "ATO0",
+            "ATO1",
+            "ATQ0",
+            "ATQ1",
+            "ATV0",
+            "ATV1",
+            "ATX0",
+            "ATX1",
+            "ATX2",
+            "ATX3",
+            "ATX4",
+            "ATZ0"});
+            this.comboBox2.Location = new System.Drawing.Point(264, 64);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(294, 21);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 50);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "300",
+            "600",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400"});
+            this.comboBox1.Location = new System.Drawing.Point(437, 171);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.Text = "9600";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(516, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Speed";
+            // 
+            // comboBoxPorts
+            // 
+            this.comboBoxPorts.FormattingEnabled = true;
+            this.comboBoxPorts.Location = new System.Drawing.Point(218, 144);
+            this.comboBoxPorts.Name = "comboBoxPorts";
+            this.comboBoxPorts.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPorts.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(481, 260);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Dissconnect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(401, 260);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Connect";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(428, 234);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(320, 260);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Pick up";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 370);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBoxPorts);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.MessageLabel);
+            this.Controls.Add(this.PeripheralLabel);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "UP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label PeripheralLabel;
+        private System.Windows.Forms.Label MessageLabel;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxPorts;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ UP-master/UP-master/UrzadzeniaPeryferyjnemodem/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace UrzadzeniaPeryferyjnemodem
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialport;
+        string receivedData;
+        delegate void A();
+        A  delegat;
+        int speed=9600;
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void WpiszOdebrane()
+        {
+            textBox1.Text+=serialport.ReadByte().ToString("X") + " ";
+        }
+        private void DataRecievedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            textBox1.Invoke(delegat);
+        }
+        private void buttonSend_Click(object sender, EventArgs e) 
+        {
+            if(serialport.IsOpen)
+            {
+                try
+                {
+                    serialport.Write(comboBox2.Text + System.Environment.NewLine);
+                    textBox1.Text += comboBox2.Text + System.Environment.NewLine;
+                }catch(Exception ex)
+                {
+                    textBox1.Text += " Error!"+ex.Message + System.Environment.NewLine;
+                }
+                serialport.DataReceived += new SerialDataReceivedEventHandler(DataRecievedHandler);
+                delegat = new A(WpiszOdebrane);
+            }
+        }
+
+       
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+
+                if (serialport.IsOpen==false)
+                {
+                    serialport.PortName = comboBoxPorts.SelectedItem.ToString();
+                    serialport.Open();
+                    textBox1.Text += " Connection open!" + System.Environment.NewLine;
+                }
+            }
+
+            /*
+    try
+    {
+        serialport = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One);//TODO: nazwa portu,szybkosc transmisji, bit parzystoci,bity danych, bit zatrzymania
+        serialport.Open();
+        textBox1.Text += " Connection open!" + System.Environment.NewLine;
+
+    }
+    */
+            catch (Exception exception)
+            {
+                textBox1.Text += "Error !" + exception.Message + System.Environment.NewLine;
+            }
+
+            
+            
+        }
+
+       /* private void dataRecived(object sender, SerialDataReceivedEventArgs e)
+        {
+            receivedData = serialport.ReadExisting();
+            if(receivedData != null)
+            {
+                BeginInvoke(new A((string text)=> { textBox1.Text += text; }), new object[] { receivedData }); 
+            }
+        }*/
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            if (serialport.IsOpen)
+            {
+                serialport.Close();
+                textBox1.Text += "Connection close !" + System.Environment.NewLine;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            speed = Int32.Parse(comboBox1.SelectedItem.ToString());
+            serialport = new SerialPort(" ", speed, Parity.None, 8, StopBits.One);//TODO: nazwa portu,szybkosc transmisji, bit parzystoci,bity danych, bit zatrzymania
+
+            String[] ports = SerialPort.GetPortNames();
+            foreach (String x in ports)
+                comboBoxPorts.Items.Add(x);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            serialport.Write("ATD "+textBox2.Text + System.Environment.NewLine);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            serialport.Write("ATA" + System.Environment.NewLine);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                serialport.Write("+" + System.Environment.NewLine);
+                System.Threading.Thread.Sleep(500);
+            }
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
