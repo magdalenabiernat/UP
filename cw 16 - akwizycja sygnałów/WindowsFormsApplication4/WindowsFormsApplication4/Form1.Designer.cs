9b7c31c1c0a348290dce4a1affe6ee1f349b78a8
Binqq <m-michal-b@wp.pl>
2017-12-17 17:58:34 +0100

    Program
    Do tego potrzebny bedzie skrypt w matlabie wczytujacy dane z pliku

---
+++ cw 16 - akwizycja sygnałów/WindowsFormsApplication4/WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bufferedAiCtrl1 = new Automation.BDaq.BufferedAiCtrl(this.components);
+            this.listBoxData = new System.Windows.Forms.ListBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bufferedAiCtrl1
+            // 
+            this.bufferedAiCtrl1._StateStream = ((Automation.BDaq.DeviceStateStreamer)(resources.GetObject("bufferedAiCtrl1._StateStream")));
+            this.bufferedAiCtrl1.DataReady += new System.EventHandler<Automation.BDaq.BfdAiEventArgs>(this.bufferedAiCtrl1_DataReady);
+            // 
+            // listBoxData
+            // 
+            this.listBoxData.FormattingEnabled = true;
+            this.listBoxData.Location = new System.Drawing.Point(86, 12);
+            this.listBoxData.Name = "listBoxData";
+            this.listBoxData.Size = new System.Drawing.Size(120, 95);
+            this.listBoxData.TabIndex = 0;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(103, 113);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.listBoxData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBoxData;
+        private System.Windows.Forms.Button buttonStart;
+        private Automation.BDaq.BufferedAiCtrl bufferedAiCtrl1;
+    }
+}
+
---
+++ cw 16 - akwizycja sygnałów/WindowsFormsApplication4/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Automation.BDaq;
+using System.IO;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        delegate void UpdateUIDelegate();
+        double[] m_dataScaled;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_dataScaled = new double[bufferedAiCtrl1.BufferCapacity];
+            bufferedAiCtrl1.Streaming = true;
+            bufferedAiCtrl1.Prepare();
+        }
+
+        private void bufferedAiCtrl1_DataReady(object sender, BfdAiEventArgs e)
+        {
+            bufferedAiCtrl1.GetData(e.Count, m_dataScaled);
+            System.IO.FileStream fs = new FileStream(Directory.GetCurrentDirectory().ToString() + "dane.txt",
+                FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            this.Invoke((UpdateUIDelegate)delegate ()
+            {
+                StreamWriter sw = new StreamWriter(fs);
+                for (int i = 0; i< e.Count; ++i)
+                {
+                    listBoxData.Items.Add(m_dataScaled[i].ToString());
+                    sw.WriteLine(m_dataScaled[i].ToString());
+                }
+                sw.Close();
+            });
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            bufferedAiCtrl1.Start();
+        }
+    }
+}
